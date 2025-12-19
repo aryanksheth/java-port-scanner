35f1b5d39134b5d7ae04cb4b57d8eab89290513a
Adu Sheth <aryanksheth@gmail.com>
2025-12-19 13:24:54 -0600

    Feature Update: New Port class, defined in comments. Added descriptions.

--- src/PortScanner.java
+++ src/PortScanner.java
@@ -6,38 +6,44 @@ import java.util.ArrayList;
 
 /*
 Implementation of port scanning. Will use Java's Socket Class
+TODO:
+ - Add MultiThreaded scanning
+ - Dynamic port scanning
+ - Create HashMaps for grabbing port descriptions
+ - Attempt to get console results (try connecting via ssh to available ports)
  */
 
 
 public class PortScanner {
-    ArrayList<Integer> openPorts = new ArrayList<>();
-    ArrayList<Integer> closedPorts = new ArrayList<>();
-    public ArrayList<Integer> portScan(String ip) throws IOException {
-
+    ArrayList<Port> portsList = new ArrayList<>();
+    public ArrayList<Port> portScan(String ip) throws IOException {
             for (int i = 0; i < 1024; i++){
+                boolean isOpen = false; //default
                 try {
-                    System.out.println("Scanning Port " + ip);
+                    System.out.println("Scanning Port " + i);
                     Socket s = new Socket();
                     s.connect(new InetSocketAddress(ip, i), 200);
                     s.close();
-                    openPorts.add(i);
+                    isOpen = true;
                 }
-                catch(Exception e){
-                    closedPorts.add(i);
-                    continue;
-                }
+                catch(Exception e){}
+                Port p = new Port(i, isOpen);
+                portsList.add(p);
             }
+        return portsList;
+        }
 
-    return openPorts; //want to change this to one list with a seperate port class that holds wether it is open or not.
-    }
-    public String toString(){
-        String result = "Open Ports: ";
-        for (int port : openPorts){
-            result += port + " ";
+
+
+    public String toString() {
+        String result = "";
+        for (Port n : portsList){
+            if (n.getisOpen()) {
+                result += n.toString() + " \n";
+            }
         }
         return result;
     }
-
 
     public static void main(String[] args) {
 //        if (args.length < 1){
@@ -45,14 +51,13 @@ public class PortScanner {
 //            return;
 //        }
 //        String targetIP = args[0];
-        String targetIP = "127.0.0.1";
+        String targetIP = "10.27.1.1";
         PortScanner ps = new PortScanner();
         try{
-           ArrayList<Integer> openPorts = ps.portScan(targetIP);
-            System.out.println("Open Ports on" + targetIP + ": " + openPorts);
+           ArrayList<Port> portScan = ps.portScan(targetIP);
+            System.out.println(ps.toString());
         } catch (IOException e) {
             System.out.println("Error scanning " + targetIP + ": " + e.getMessage());
         }
-
     }
-}
+    }
